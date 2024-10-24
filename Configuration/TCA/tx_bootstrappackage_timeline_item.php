1d9ed928b2009384400fc3697a7a0b0d00a9c1e0
StephanSellner <13609111+StephanSellner@users.noreply.github.com>
2024-10-24 10:05:48 +0200

    Update tx_bootstrappackage_timeline_item.php

--- Configuration/TCA/tx_bootstrappackage_timeline_item.php
+++ Configuration/TCA/tx_bootstrappackage_timeline_item.php
@@ -221,13 +221,6 @@ return [
                 'appearance' => [
                     'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/Database.xlf:tt_content.asset_references.addFileReference',
                 ],
-                'overrideChildTca' => [
-                    'types' => [
-                        \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
-                            'showitem' => '--palette--;;filePalette',
-                        ],
-                    ],
-                ],
                 'minitems' => 0,
                 'maxitems' => 1,
             ],
@@ -241,15 +234,6 @@ return [
                 'allowed' => 'common-image-types',
                 'minitems' => 0,
                 'maxitems' => 1,
-                'overrideChildTca' => [
-                    'types' => [
-                        \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
-                            'showitem' => '
-                                --palette--;;imageoverlayPalette,
-                                --palette--;;filePalette',
-                        ],
-                    ],
-                ],
             ],
         ],
     ],
